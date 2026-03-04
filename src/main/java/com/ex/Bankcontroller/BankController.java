f86b7a9c2c6b3f84d85edeaef46e153ac5affdce
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2026-03-04 11:57:26 +0530

    Basic authentication

---
+++ src/main/java/com/ex/Bankcontroller/BankController.java
@@ -0,0 +1,35 @@
+package com.ex.Bankcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BankController
+{
+
+    @GetMapping("/balance")
+    public String balanace()
+    {
+        return "Your Balance :: 7884.00 INR";
+    }
+
+    @GetMapping("/transfer")
+    public String amtTransfer()
+    {
+        return "Your Amount Transfered Successfullly" ;
+    }
+
+    @GetMapping("/login-check")
+    public String login()
+    {
+        return "login sucessfully";
+    }
+
+    @GetMapping("/contact")
+    public String getContactInfo()
+    {
+        return "Contact US :: abc@bank.com";
+    }
+
+}
---
+++ src/main/java/com/ex/Config/SecuirtyConfig.java
@@ -0,0 +1,31 @@
+package com.ex.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecuirtyConfig
+{
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception
+    {
+       //logic to custamize the secrity in our app
+
+        http.authorizeHttpRequests
+                ((request)-> request.requestMatchers("/","/login-check","/contact","/swagger-ui.html")
+                                .permitAll()
+                                .anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults()
+        );
+        return http.build();
+    }
+
+
+
+}
